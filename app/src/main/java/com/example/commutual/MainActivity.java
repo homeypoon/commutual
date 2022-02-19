e0a173858fb2dde438f07b381b1357dc4666ff13
nandinideviparimi <nandinicbit@gmail.com>
2022-02-19 00:50:18 -0500

    intermediate commit

--- app/src/main/java/com/example/commutual/MainActivity.java
+++ app/src/main/java/com/example/commutual/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.commutual;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+//                findViewById(R.id.bottom_navigation);
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(myNavigationItemListener);
+//        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+//        bottomNav.setOnNavigationItemSelectedListener(navListener);
+//
+//        // as soon as the application opens the first
+//        // fragment should be shown to the user
+//        // in this case it is algorithm fragment
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AlgorithmFragment()).commit();
     }
-}
+
+//    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            // By using switch we can easily get
+//            // the selected fragment
+//            // by using there id.
+//            Fragment selectedFragment = null;
+//            switch (item.getItemId()) {
+//                case R.id.algorithm:
+//                    selectedFragment = new AlgorithmFragment();
+//                    break;
+//                case R.id.course:
+//                    selectedFragment = new CourseFragment();
+//                    break;
+//                case R.id.profile:
+//                    selectedFragment = new ProfileFragment();
+//                    break;
+//            }
+//            // It will help to replace the
+//            // one fragment to other.
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.fragment_container, selectedFragment)
+//                    .commit();
+//            return true;
+//        }
+//    };
+
+    }

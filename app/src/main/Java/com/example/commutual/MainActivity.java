520adef28c3c1482b3eb8edfaa9b013e67424b00
homeypoon <pn.homey@gmail.com>
2022-02-28 21:35:51 -0500

    Added 4 navigation tabs. Switching between tabs works fine.

--- app/src/main/Java/com/example/commutual/MainActivity.java
+++ app/src/main/Java/com/example/commutual/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.commutual;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,31 +35,31 @@ public class MainActivity extends AppCompatActivity {
 //        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AlgorithmFragment()).commit();
     }
 
-//    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            // By using switch we can easily get
-//            // the selected fragment
-//            // by using there id.
-//            Fragment selectedFragment = null;
-//            switch (item.getItemId()) {
-//                case R.id.algorithm:
-//                    selectedFragment = new AlgorithmFragment();
-//                    break;
-//                case R.id.course:
-//                    selectedFragment = new CourseFragment();
-//                    break;
-//                case R.id.profile:
-//                    selectedFragment = new ProfileFragment();
-//                    break;
-//            }
-//            // It will help to replace the
-//            // one fragment to other.
-//            getSupportFragmentManager()
-//                    .beginTransaction()
-//                    .replace(R.id.fragment_container, selectedFragment)
-//                    .commit();
-//            return true;
-//        }
-//    };
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // By using switch we can easily get
+            // the selected fragment
+            // by using there id.
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.explore_page:
+                    selectedFragment = new ExploreFragment();
+                    break;
+                case R.id.chat_page:
+                    selectedFragment = new ExploreFragment();
+                    break;
+                case R.id.profile_page:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+            // It will help to replace the
+            // one fragment to other.
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+            return true;
+        }
+    };
     }
